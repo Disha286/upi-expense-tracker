3c85ff643ca9c400253e6cab9c3552be73bbdfb1
Disha <disharavi286@gmail.com>
2026-02-28 23:34:42 +0530

    what you changed

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../db"); // DB connection
+const pool = require("../db");
 
-// POST /transactions -> Add new transaction
+// POST /transactions
 router.post("/", async (req, res) => {
   try {
     const { user_id, category_id, amount, note, transaction_date } = req.body;
 
-    const newTransaction = await pool.query(
-      `INSERT INTO transactions 
-       (user_id, category_id, amount, note, transaction_date)
+    const result = await pool.query(
+      `INSERT INTO transactions (user_id, category_id, amount, note, transaction_date)
        VALUES ($1, $2, $3, $4, $5)
        RETURNING *`,
       [user_id, category_id, amount, note, transaction_date]
     );
 
-    res.json(newTransaction.rows[0]);
+    res.json(result.rows[0]);
   } catch (err) {
-    console.error(err.message);
+    console.error(err);
     res.status(500).send("Server Error");
   }
 });
